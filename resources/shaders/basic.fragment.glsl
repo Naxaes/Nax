6b40eb883030afe8ae70de8c2528860e62703774
Kleinman <ted.kleinman@hotmail.com>
2019-02-18 00:16:41 +0100

    Added sunlight and configuration in widget.

--- resources/shaders/basic.fragment.glsl
+++ resources/shaders/basic.fragment.glsl
@@ -8,20 +8,21 @@ in Shared {
 
 
 uniform vec3 color;
+uniform vec3 sunlight_direction;
 
 out vec3 out_color;
 
 
 void main()
 {
-    // Hardcoded for now.
-    vec3 sunlight_direction = normalize(vec3(1.0f, 1.0f, 1.0f));
+    // Pedantic safety.
+    vec3 normalized_sunlight_direction = normalize(sunlight_direction);
 
     // Ambient light.
     vec3 ambient = color * 0.2f;
 
     // Specular light.
-    float specular_factor = max(dot(fs_in.normal, sunlight_direction), 0);
+    float specular_factor = max(dot(fs_in.normal, normalized_sunlight_direction), 0);
     vec3  specular = color * specular_factor * 0.8f;
 
     out_color = ambient + specular;
